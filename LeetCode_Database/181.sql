6db049bc3b98d6e683e640c1c100554926015cb8
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-03-08 17:07:00 -0500

    Update 181.sql

--- LeetCode_Database/181.sql
+++ LeetCode_Database/181.sql
@@ -5,3 +5,10 @@ from Employee as e1
 inner join Employee as e2 
 on e1.ManagerId=e2.Id 
 where e1.Salary>e2.Salary 
+
+
+# A faster solution
+
+select E1.Name 
+from Employee as E1, Employee as E2 
+where E1.ManagerId = E2.Id and E1.Salary > E2.Salary
